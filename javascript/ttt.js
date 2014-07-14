f8bd8b90b2c3790bac061d009a86d3d353463aef
Dmitry Samsonau <demn_@tut.by>
2014-07-14 14:02:44 +0300

    "js lint -ing" code

--- javascript/ttt.js
+++ javascript/ttt.js
@@ -1,47 +1,55 @@
 function inherit(C, P) {
-    var F = function () {};
+    'use strict';
+    var F = function () { };
     F.prototype = P.prototype;
     C.prototype = new F();
     C.uber = P.prototype;
     C.prototype.constructor = C;
-};
+}
 
 function Player(name) {
     this.name = name;
-};
+}
 
 function ComputerPlayer(name) {
     inherit(ComputerPlayer, Player);
-};
+    this.name = name;
+}
 
-var Playboard = function(){
-    var element = document.getElementById('board');
+var Playboard = function () {
+    'use strict';
+    //var element = document.getElementById('board'),
+    //    i = 0;
 
-    this.update = function (itemPos) {
-        var str;
-        for (var i = 0; i < array.length; i++) {
-            str += array[i];
-        }
-        this.element.innerHTML = str;
-    }
+    //this.update = function (itemPos) {
+    //    var str = '';
+    //    for (i = 0; i < array.length; i++) {
+    //        str += array[i];
+    //    }
+    //    this.element.innerHTML = str;
+    //}
 };
 
 var Mediator = function () {
-    var isFinished = false,
-    movesNumber = 0,
-    currMovePlayer1 = true,
-    players = {}
-    that = this;
+    'use strict';
+    var i = 0,
+        button,
+        isFinished = false,
+        movesNumber = 0,
+        currMovePlayer1 = true,
+        players = {},
+        that = this;
 
     that.setup = function () {
         players.first = new Player('First');
         players.second = new ComputerPlayer('Second');
 
         var buttons = document.getElementsByTagName('button');
-        for (var i = 0; i < buttons.length; i++) {
-            var button = buttons[i];
+        for (i = 0; i < buttons.length; i += 1) {
+            button = buttons[i];
             button.onclick = that.buttonpress;
         }
+        return that;
     },
 
     that.played = function (el) {
@@ -62,18 +70,21 @@ var Mediator = function () {
             isFinished = true;
             that.finish();
         }
+        return that;
     },
 
     that.buttonpress = function (e) {
         var el = e.currentTarget;
         that.played(el);
+        return that;
     },
 
      that.finish = function () {
          var title = document.getElementById("title");
          title.innerHTML = "The game is finished";
          alert("the winner is i don't know who");
-     }
+         return that;
+     };
 };
 
 
