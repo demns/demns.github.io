6133876d37d6a9908763b935329def9a3b9fe650
Dmitry Samsonau <demn_@tut.by>
2014-07-18 14:15:29 +0300

    0.0.2.2 jslint+jshint changes

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,112 +1,125 @@
-var MyApp = new Backbone.Marionette.Application();
-MyApp.globalVars = {};
-MyApp.globalVars.currMove = 0;
-MyApp.globalVars.currArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-MyApp.globalVars.winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-MyApp.globalVars.appendIndex = 0;
-MyApp.globalVars.theWinnerIs = false;
-MyApp.globalVars.checkForWin = function () {
-    var arr = MyApp.globalVars.currArray;
+(function () {
+    "use strict";
+    var myApp = new window.Backbone.Marionette.Application();
+    myApp.globalVars = {};
+    myApp.globalVars.currMove = 0;
+    myApp.globalVars.currArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    myApp.globalVars.winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    myApp.globalVars.appendIndex = 0;
+    myApp.globalVars.theWinnerIs = false;
+    myApp.globalVars.checkForWin = function () {
+        var arr = myApp.globalVars.currArray,
+            i = 0,
+            isWinningCombination = function () {
+                var winnComb = myApp.globalVars.winningCombinations,
+                    length = winnComb.length;
 
-    var isWinningCombination = function () {
-        var winningCombinations = MyApp.globalVars.winningCombinations,
-            length = winningCombinations.length;
+                for (i = 0; i < length; i++) {
+                    if (arr[winnComb[i][0]] ===
+                        arr[winnComb[i][1]] && arr[winnComb[i][1]] ===
+                        arr[winnComb[i][2]]) {
+                        return arr[winnComb[i][2]];
+                    }
 
-        for (var j = 0; j < length; j++) {
-            if (arr[winningCombinations[j][0]] === 
-                arr[winningCombinations[j][1]] && arr[winningCombinations[j][1]] ===
-                arr[winningCombinations[j][2]])
-                return arr[winningCombinations[j][2]];
+                }
+                return false;
+            },
+            isWin = isWinningCombination();
+
+
+        if (isWin !== false) {
+            myApp.globalVars.theWinnerIs = isWin;
+            return true;
         }
-        return false;
-    };
 
-    var isWin = isWinningCombination();
-    if (isWin !== false) {
-        MyApp.globalVars.theWinnerIs = isWin;
-        return true;
-    }
-
-    for (var i = 0; i < 9; i++) {
-        if (arr[i] !== 'X' && arr[i] !== 'Y') {
-            return false;
-        }
-    }
-
-    MyApp.globalVars.theWinnerIs = "Draw";
-    return true;
-};
-
-
-MyApp.addRegions({
-    mainRegion: "#content"
-});
-
-var Cell = Backbone.Model.extend({});
-
-var Cells = Backbone.Collection.extend({
-    model: Cell
-});
-
-
-var CellView = Backbone.Marionette.ItemView.extend({
-    template: "#cell-template",
-    tagName: 'td',
-    className: 'cell',
-    events: {
-        'click': 'changeState'
-    },
-
-    changeState: function() {
-        if (MyApp.globalVars.theWinnerIs === false && this.el.firstElementChild.innerHTML !== 'X' && this.el.firstElementChild.innerHTML !== 'Y') {
-            var ch = MyApp.globalVars.currMove++ % 2 === 0 ? 'X' : 'Y';
-            this.el.firstElementChild.innerHTML = ch;
-            MyApp.globalVars.currArray[parseInt(this.el.firstElementChild.id[2], 10) - 1] = ch;
-            var winnerField = $("#winner");
-            if (MyApp.globalVars.checkForWin()) {
-                winnerField.text("The winner is: " + MyApp.globalVars.theWinnerIs);
+        for (i = 0; i < 9; i++) {
+            if (arr[i] !== 'X' && arr[i] !== 'Y') {
+                return false;
             }
         }
-    },
-});
 
+        myApp.globalVars.theWinnerIs = "Draw";
+        return true;
+    };
 
-
-
-var CellsView = Backbone.Marionette.CompositeView.extend({
-    tagName: "tr",
-    id: "cells",
-    className: "table table-bordered",
-    template: "#cells-template",
-    itemView: CellView,
-
-    appendHtml: function(collectionView, itemView) {
-        if (MyApp.globalVars.appendIndex++ % 3 === 0) {
-            collectionView.$("tbody").append("<tr></tr>");
-        }
-        collectionView.$("tbody").append(itemView.el);
-    }
-});
-
-MyApp.addInitializer(function(options) {
-    var cellsView = new CellsView({
-        collection: options.cells
+    myApp.addRegions({
+        mainRegion: "#content"
     });
-    MyApp.mainRegion.show(cellsView);
-});
 
-$(document).ready(function() {
-    var cells = new Cells([
-        new Cell({ name: '1' }),
-        new Cell({ name: '2' }),
-        new Cell({ name: '3' }),
-        new Cell({ name: '4' }),
-        new Cell({ name: '5' }),
-        new Cell({ name: '6' }),
-        new Cell({ name: '7' }),
-        new Cell({ name: '8' }),
-        new Cell({ name: '9' })
-    ]);
+    myApp.addInitializer(function (options) {
+        var cellsView = new CellsView({
+            collection: options.cells
+        });
+        myApp.mainRegion.show(cellsView);
+    });
 
-    MyApp.start({ cells: cells });
-});
+
+    var Cell = window.Backbone.Model.extend({}),
+
+        Cells = window.Backbone.Collection.extend({
+            model: Cell
+        }),
+
+
+        CellView = window.Backbone.Marionette.ItemView.extend({
+            template: "#cell-template",
+            tagName: 'td',
+            className: 'cell',
+            events: {
+                'click': 'changeState'
+            },
+
+            changeState: function () {
+                var elem = this.el.firstElementChild,
+                    ch = myApp.globalVars.currMove++ % 2 === 0 ? 'X' : 'Y',
+                    winnerField = $("#winner");
+                if (myApp.globalVars.theWinnerIs === false &&
+                    elem.innerHTML !== 'X' &&
+                    elem.innerHTML !== 'Y') {
+
+                    elem.innerHTML = ch;
+                    myApp.globalVars.currArray[parseInt(elem.id[2], 10) - 1] = ch;
+
+                    if (myApp.globalVars.checkForWin()) {
+                        winnerField.text("The winner is: " + myApp.globalVars.theWinnerIs);
+                    }
+                }
+            }
+        }),
+
+
+        CellsView = window.Backbone.Marionette.CompositeView.extend({
+            tagName: "tr",
+            id: "cells",
+            className: "table table-bordered",
+            template: "#cells-template",
+            itemView: CellView,
+
+            appendHtml: function (collectionView, itemView) {
+                if (myApp.globalVars.appendIndex++ % 3 === 0) {
+                    collectionView.$("tbody").append("<tr></tr>");
+                }
+                collectionView.$("tbody").append(itemView.el);
+            }
+        });
+
+
+    $(document).ready(function () {
+        var cells = new Cells(),
+            i;
+        for (i = 1; i < 10; i++) {
+            cells.push(new Cell({ name: i }));
+        }
+
+        myApp.start({ cells: cells });
+    });
+}());
