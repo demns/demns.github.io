6c306bad190c3a6679d24708589d38cc2cf57ee4
Dmitry Samsonau <demn_@tut.by>
2014-08-13 17:49:50 +0300

    refactoring logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,17 +1,12 @@
-// <svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">
-//             <line x1="0" y1="0" x2="10" y2="10" style="stroke:purple;stroke-width:2" />
-//             <line x1="0" y1="10" x2="10" y2="0" style="stroke:purple;stroke-width:2" />
-//           </svg>
+var globalVars = {
+    count: 0,
+    svgCircle: '<circle cx="5" cy="5" r="4" stroke="black" stroke-width="2" fill-opacity="0" />',
+    svgCross: '<line x1="0" y1="0" x2="10" y2="10" style="stroke:purple;stroke-width:2" />' +
+        '<line x1="0" y1="10" x2="10" y2="0" style="stroke:purple;stroke-width:2" />',
+    svgOpenTag: '<svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">',
+    svgCloseTag: '</svg>'
+};
 
-// <svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">
-//             <circle cx="5" cy="5" r="4" stroke="black" stroke-width="2" fill-opacity="0" />
-//           </svg>
-
-// <svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">
-
-//           </svg>
-
-var count = 0;
 (function() {
     Cell = Backbone.Model.extend({
 
@@ -29,25 +24,19 @@ var count = 0;
             'click': 'changeState'
         },
         changeState: function() {
-            if (count > 9 || this.el.innerHTML.indexOf('line') > 0 || this.el.innerHTML.indexOf('circle') > 0) {
+            if (globalVars.count > 9 || this.el.innerHTML.indexOf('line') > 0 || this.el.innerHTML.indexOf('circle') > 0) {
                 return;
             }
-            if (count++ % 2 === 0) {
-                this.el.innerHTML = '<svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">' +
-                    '<line x1="0" y1="0" x2="10" y2="10" style="stroke:purple;stroke-width:2" />' +
-                    '<line x1="0" y1="10" x2="10" y2="0" style="stroke:purple;stroke-width:2" />' +
-                    '</svg>';
+            if (globalVars.count++ % 2 === 0) {
+                this.el.innerHTML = globalVars.svgOpenTag + globalVars.svgCross + globalVars.svgCloseTag;
             } else {
-                this.el.innerHTML = '<svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;">' +
-                    '<circle cx="5" cy="5" r="4" stroke="black" stroke-width="2" fill-opacity="0" />' +
-                    '</svg>';
+                this.el.innerHTML = globalVars.svgOpenTag + globalVars.svgCircle + globalVars.svgCloseTag;
             }
 
         },
 
         render: function() {
-            $(this.el).html('<svg width="50%" height="50%" viewBox="0 0 10 10" style="display: block; margin: 0 auto;"></svg>' +
-                _.template($('#cell-template').html(), {}));
+            $(this.el).html(globalVars.svgOpenTag + globalVars.svgCloseTag + _.template($('#cell-template').html(), {}));
             return this;
         }
     });
