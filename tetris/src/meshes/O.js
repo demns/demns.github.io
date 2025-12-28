c51a047395a1feb86f7fb273fb791147eb445f30
demns <demn_@tut.by>
2025-12-28 10:32:35 +0100

    Fix O piece coordinates to use fractional positions

--- tetris/src/meshes/O.js
+++ tetris/src/meshes/O.js
@@ -2,9 +2,9 @@ import { createTetrisMesh } from './baseMesh';
 
 export default function getOMesh() {
 	return createTetrisMesh([
-		[0, 0, 0],
-		[1, 0, 0],
-		[0, 1, 0],
-		[1, 1, 0]
+		[-0.5, -0.5, 0],
+		[0.5, -0.5, 0],
+		[-0.5, 0.5, 0],
+		[0.5, 0.5, 0]
 	]);
 }
